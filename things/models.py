5cdf8867f120c7bca6f6c92b7b697569ba15185b
Sahitya Sakthivel <sahitya.sakthivel@kcl.ac.uk>
2022-10-03 18:09:49 +0100

    Added restriction on parameters

--- things/models.py
+++ things/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class Thing(models.Model):
-    name =models.CharField(max_length = 30, unique = True)
-    description =models.CharField(max_length = 120, unique = False)
-    quantity = models.IntegerField()
+    name =models.CharField(max_length = 30, unique = True, blank = False)
+    description =models.CharField(max_length = 120, unique = False, blank = True)
+    quantity = models.IntegerField(unique = False,
+            validators = [MaxValueValidator(100),MinValueValidator(0)])
