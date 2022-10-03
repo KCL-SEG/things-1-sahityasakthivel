a6c463e337087f61b0f734ed78771f23a5d42e7d
Sahitya Sakthivel <sahitya.sakthivel@kcl.ac.uk>
2022-10-03 18:00:03 +0100

    Added restriction on parameters

--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
-class Thing(models.Model):
+class Thing():
     name =models.CharField(max_length = 30, unique = True)
     description =models.CharField(max_length = 120, unique = False)
     quantity = models.IntegerField()
--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+class UnitTestCase(TestCase):
+    def test(self):
+        a = Thing(name = "pen", description = "writing",quantity = 2);
+        try:
+             a.full_clean()
+        except (ValidationError):
+            self.fail("FAiled")
