c3890a79e30b14b6e44ddb4bf1ddf36dd1980a5a
Sahitya Sakthivel <sahitya.sakthivel@kcl.ac.uk>
2022-10-03 17:41:07 +0100

    Added restriction on parameters

--- things/models.py
+++ things/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class Thing(models.Model):
-    name =models.CharField(max_length = 50)
-    description =models.CharField(max_length = 100)
-    quantity = models.IntegerField()
+    name =models.CharField(max_length = 30, unique = True,  blank = False)
+    description =models.CharField(max_length = 120,  unique = False, blank = True)
+    quantity = models.IntegerField(unique = False,
+            validators = [MaxValueValidator(100),MinValueValidator(0)])
